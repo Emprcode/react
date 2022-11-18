d3a4344e5b4821ffc11eee8ead5824cb140235c5
Narayan Aryal <aryal.coding@gmail.com>
2022-11-19 00:34:22 +1100

    Movie card Created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { SearchForm } from "./components/SearchForm";
+import { MovieCard } from "./components/MovieCard";
 
 function App() {
   return (
     <div className="App">
       <h2 className="d-flex justify-content-center mt-5">Movie List</h2>
       <SearchForm />
+      <MovieCard />
     </div>
   );
 }
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+
+export const MovieCard = () => {
+  return (
+    <div>
+      <Card
+        className="d-flex justify-content-center"
+        style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
