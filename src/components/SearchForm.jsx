5fc3fb9bb2b698266656789be11029041b41f233
Narayan Aryal <aryal.coding@gmail.com>
2022-11-20 20:54:06 +1100

    More code added

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
 
 export const SearchForm = () => {
+  const [form, setForm] = useState("");
+
   const handleOnChange = (e) => {
     const { value } = e.target;
-    console.log(value);
+    setForm(value);
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
   };
 
   return (
     <div className="d-flex justify-content-center p-5">
-      <Form>
+      <Form onSubmit={handleOnSubmit}>
         <Row>
           <Col>
             <Form.Control onChange={handleOnChange} placeholder="Movie Name" />
           </Col>
           <Col>
-            <Button variant="primary">Search</Button>{" "}
+            <Button type="submit" variant="primary">
+              Search
+            </Button>{" "}
           </Col>
         </Row>
       </Form>
