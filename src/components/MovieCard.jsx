e4e82b0343687388e8fd6fe794f5bbd46ed781a4
Narayan Aryal <aryal.coding@gmail.com>
2022-11-26 22:18:19 +1100

    Card redesigned

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 export const MovieCard = ({ movie = {} }) => {
   console.log(movie);
   const { Poster, Title, Genre, Plot } = movie;
   return (
-    <div>
-      <Card
-        className="d-flex justify-content-center"
-        style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={Poster} />
-        <Card.Body>
-          <Card.Title>{Title}</Card.Title>
-          <Card.Text>{Genre}</Card.Text>
-          <Card.Text>{Plot}</Card.Text>
-        </Card.Body>
-      </Card>
-    </div>
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={Poster} />
+      <Card.Body>
+        <Card.Title>{Title}</Card.Title>
+        <Card.Text>{Genre}</Card.Text>
+        <Card.Text>{Plot}</Card.Text>
+        <Button variant="primary">Happy</Button>
+      </Card.Body>
+    </Card>
   );
 };
