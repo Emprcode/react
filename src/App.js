d3ef0273b19492702a730def4640a5f7f91163c9
Narayan Aryal <aryal.coding@gmail.com>
2022-11-30 13:21:17 +1100

    code cleaned

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { SearchForm } from "./components/SearchForm";
-import { MovieCard } from "./components/MovieCard";
 import { MovieList } from "./components/MovieList";
 
 function App() {
