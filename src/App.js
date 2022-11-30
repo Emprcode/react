d8505cc2127d0f1c5d7bdf029f79b12277da72b0
Narayan Aryal <aryal.coding@gmail.com>
2022-11-30 13:19:53 +1100

    more code added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { SearchForm } from "./components/SearchForm";
 import { MovieCard } from "./components/MovieCard";
+import { MovieList } from "./components/MovieList";
 
 function App() {
   return (
     <div className="App">
       <h2 className="d-flex justify-content-center mt-5">Movie List</h2>
       <SearchForm />
+      <MovieList />
     </div>
   );
 }
